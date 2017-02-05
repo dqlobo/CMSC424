e8fcb19dc67243251fcbae11cec4a34da485d7c1
dlobosco <davidlobosco@gmail.com>
2017-02-04 22:40:26 -0500

    finished project

---
+++ Assets/Controllers/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Finish")) {
+			Vector3 upForce = new Vector3 (0, 7500, 0);
+			rb.AddForce (upForce);
+		}
+	}
+}
